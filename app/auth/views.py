39680c99145db9319d4095ac12bdd7f70f7f6726
blackrose <wahomekelving@gmail.com>
2019-02-18 11:26:08 +0300

    Created a login view function

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,6 @@
-from flask import Blueprint
+from flask import render_template
+from . import auth
 
-auth = Blueprint('auth',__name___)
-
-from . import views
+@auth.route('/login')
+def login():
+    return render_template('auth/login.html')

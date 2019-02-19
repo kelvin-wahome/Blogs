15b09a57a5a27e1d9b7629ad011a65960a441c5b
blackrose <wahomekelving@gmail.com>
2019-02-19 16:05:24 +0300

    Deployed

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get("HEROKU_POSTGRESQL_PUCE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 class DevConfig(Config):
